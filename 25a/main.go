02cdec026cebba92ab6eb3fba4b3f2706573ca42
Carson Anderson <ca@carsonoid.net>
2019-12-25 21:08:54 -0700

    Complete 25a. Semi-auto solver with inv + auto

--- 25a/main.go
+++ 25a/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	combinations "github.com/mxschmitt/golang-combinations"
 )
 
 var inputInts = []int{}
@@ -16,6 +18,7 @@ var x = xDim / 2
 var y = yDim / 2
 var lastIn string
 var lastOut string
+var items []string
 
 func draw() {
 	for i := 0; i < yDim; i++ {
@@ -66,12 +69,80 @@ func input(c *computer) int {
 		}
 		fmt.Println()
 
+		fmt.Println("text", text, "lastin", lastIn)
+		if lastIn == "auto\n" {
+			items = getItems(lastOut)
+			text = getPermutationTestCode(items)
+		}
+
 		for _, r := range text {
 			inputInts = append(inputInts, int(r))
 		}
 	}
 	lastOut = ""
 	return autoInput()
+}
+
+func permutations(arr []string) [][]string {
+	var helper func([]string, int)
+	res := [][]string{}
+
+	helper = func(arr []string, n int) {
+		if n == 1 {
+			tmp := make([]string, len(arr))
+			copy(tmp, arr)
+			res = append(res, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				helper(arr, n-1)
+				if n%2 == 1 {
+					tmp := arr[i]
+					arr[i] = arr[n-1]
+					arr[n-1] = tmp
+				} else {
+					tmp := arr[0]
+					arr[0] = arr[n-1]
+					arr[n-1] = tmp
+				}
+			}
+		}
+	}
+	helper(arr, len(arr))
+	return res
+}
+
+var direction string = "west"
+
+func getPermutationTestCode(items []string) string {
+	var code []string
+	// first drop all
+	for _, item := range items {
+		code = append(code, fmt.Sprintf("drop %s", item))
+	}
+	// then try all combos
+	for _, comb := range combinations.All(items) {
+		for _, item := range comb {
+			code = append(code, fmt.Sprintf("take %s", item))
+		}
+		code = append(code, "west")
+		for _, item := range comb {
+			code = append(code, fmt.Sprintf("drop %s", item))
+		}
+	}
+	fmt.Println(strings.Join(code, "\n"))
+	return strings.Join(code, "\n")
+}
+
+func getItems(s string) []string {
+	fmt.Println("RAW", s)
+	items = []string{}
+	for _, line := range strings.Split(s, "\n") {
+		if strings.HasPrefix(line, "- ") {
+			items = append(items, strings.TrimLeft(line, "- "))
+		}
+	}
+	fmt.Println("ITEMS", items)
+	return items
 }
 
 func autoInput() int {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/jmcvetta/randutil v0.0.0-20150817122601-2bb1b664bcff
+	github.com/mxschmitt/golang-combinations v1.0.0
 	github.com/prometheus/common v0.7.0
 	k8s.io/api v0.0.0-20191121015604-11707872ac1c
 	k8s.io/apimachinery v0.0.0-20191123233150-4c4803ed55e3
